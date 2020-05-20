1e91e654b755951e3bb142a0e69f2f3beae0c908
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-20 16:48:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,23 +1,4 @@
-def join_nested_strings(src)
-  
-  result=[]
-  ind=0 
-  inner_ind=0
-  while ind<src.length do
-  while inner_ind<src[ind].length do 
-    if inner_ind.is_a?String
-      puts "cats"
-      #result << src[ind][inner_ind]
-    end
-    inner_ind+=1 
-  end 
-  ind+=1 
-end
-result
-end
 
-join_nested_string([1,"h"],["g",34])
-  
   
   
   # src will be an Array of Arrays of Strings and Integers
